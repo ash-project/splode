f6d92632f4f5eb9a77ad57fcb2c390ecfbfbe096
Zach Daniel <zach@zachdaniel.dev>
2024-04-05 19:31:55 -0400

    fix: loosen elixir language version requirement

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Splode.MixProject do
     [
       app: :splode,
       version: @version,
-      elixir: "~> 1.16",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
