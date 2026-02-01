44789b27a93bb1d894fd332e38146a75943827d2
Leon Qadirie <leon.qadirie@nulltree.xyz>
2026-02-01 16:14:29 +0100

    chore: SPDX contributor links

--- .doctor.exs
+++ .doctor.exs
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 splode contributors <https://github.com/ash-project/splode/graphs.contributors>
+# SPDX-FileCopyrightText: 2024 splode contributors <https://github.com/ash-project/splode/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
